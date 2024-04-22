f835352caf396a96838aaf48570648f0dcb53cef
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-22 14:00:18 -0700

    added placeholder component

---
+++ jobly-frontend/src/components/Placeholder/Placeholder.tsx
@@ -0,0 +1,11 @@
+
+function Placeholder () {
+  return (
+    <div className="">
+      <h1>Placeholder</h1>
+      <p>This is a placeholder component</p>
+    </div>
+  )
+}
+
+export default Placeholder
