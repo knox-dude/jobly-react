d58e57fdbbbeffb1ddbc0826968fd2af6cea6b3c
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-23 11:59:36 -0700

    started removing placeholders and adding components

--- jobly-frontend/src/components/AppRoutes/AppRoutes.tsx
+++ jobly-frontend/src/components/AppRoutes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Placeholder from "@/components/Placeholder/Placeholder";
+import CompanyList from "@/components/CompanyList/CompanyList";
+import CompanyDetail from "@/components/CompanyDetail/CompanyDetail";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Placeholder />} />
-      <Route path="/companies" element={<Placeholder />} />
-      <Route path="/companies/:handle" element={<Placeholder />} />
+      <Route path="/companies" element={<CompanyList />} />
+      <Route path="/companies/:handle" element={<CompanyDetail />} />
       <Route path="/jobs" element={<Placeholder />} />
       <Route path="/login" element={<Placeholder />} />
       <Route path="/signup" element={<Placeholder />} />
