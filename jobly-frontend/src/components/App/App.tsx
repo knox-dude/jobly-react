b764acb232c858cfeb3dd72be6a2c84b3b076f9d
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-23 15:15:56 -0700

    removed old logic from App that was already implemented in CompanyList

--- jobly-frontend/src/components/App/App.tsx
+++ jobly-frontend/src/components/App/App.tsx
@@ -1,52 +1,13 @@
-import { useEffect, useState } from "react";
-import { JoblyApi } from "@/api";
-import { Company } from "@/types";
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "../Navbar/NavBar";
 import AppRoutes from "../AppRoutes/AppRoutes";
 
 function App() {
-  const [companies, setCompanies] = useState<Company[]>([]);
-
-  useEffect(() => {
-    const fetchCompanies = async () => {
-      try {
-        const response = await JoblyApi.getCompanies();
-        setCompanies(response);
-      } catch (error) {
-        console.error("Error fetching companies:", error);
-      }
-    };
-    fetchCompanies();
-  }, []);
-
-  const renderCompanies = () => {
-    return companies.map((company) => {
-      return (
-        <div key={company.handle}>
-          <h2>{company.name}</h2>
-          <p>{company.description}</p>
-        </div>
-      );
-    });
-  };
-
   return (
     <div className="App">
       < BrowserRouter>
       < NavBar />
       < AppRoutes />
-        <header className="App-header">
-          <p>{renderCompanies()}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </BrowserRouter>
     </div>
   );
