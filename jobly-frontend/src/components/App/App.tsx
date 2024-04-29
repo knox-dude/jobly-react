ba6887aea6bd79e6b5d9079bdf394c1b9c93f755
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-29 15:55:11 -0700

    adding some comments

--- jobly-frontend/src/components/App/App.tsx
+++ jobly-frontend/src/components/App/App.tsx
@@ -3,11 +3,14 @@ import NavBar from "../Navbar/NavBar";
 import AppRoutes from "../AppRoutes/AppRoutes";
 
 function App() {
+
+  // TODO: add retrieval of token from local storage, updating of nav based on token
+
   return (
     <div className="App">
-      < BrowserRouter>
-      < NavBar />
-      < AppRoutes />
+      < BrowserRouter >
+        < NavBar />
+        < AppRoutes />
       </BrowserRouter>
     </div>
   );
