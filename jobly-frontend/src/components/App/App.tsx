7fbd1155d9bd0264a3a146588420986c538b91c1
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-08 16:34:33 -0700

    i dont fuckin know

--- jobly-frontend/src/components/App/App.tsx
+++ jobly-frontend/src/components/App/App.tsx
@@ -3,11 +3,19 @@ import NavBar from "../Navbar/NavBar";
 import AppRoutes from "../AppRoutes/AppRoutes";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import TokenContext from "../TokenContext/TokenContext";
+import { JoblyApi } from "@/api";
+// import { useEffect } from "react";
 
 function App() {
 
   // retrieves token, setToken from local storage and passes it down via TokenContext
   const [token, setToken] = useLocalStorage<string | null>('jobly_token', null);
+
+  JoblyApi.token = token;
+  // useEffect(() => {
+  //   JoblyApi.token = token;
+  // }, [token]);
+
 
   return (
     <TokenContext.Provider value={[token, setToken]}>
