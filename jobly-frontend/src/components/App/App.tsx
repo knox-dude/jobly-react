8311425f8577393c61276cfda81898d9ac63b855
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-09 15:54:40 -0700

    changed the context from a single token to user, logout, login, signup

--- jobly-frontend/src/components/App/App.tsx
+++ jobly-frontend/src/components/App/App.tsx
@@ -2,30 +2,81 @@ import { BrowserRouter } from "react-router-dom";
 import NavBar from "../Navbar/NavBar";
 import AppRoutes from "../AppRoutes/AppRoutes";
 import useLocalStorage from "@/hooks/useLocalStorage";
-import TokenContext from "../TokenContext/TokenContext";
+import CurrUserContext from "../CurrUserContext/CurrUserContext";
 import { JoblyApi } from "@/api";
-// import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { User } from "@/types";
+import { jwtDecode } from "jwt-decode";
+
+type LoginParams = { username: string; password: string };
+type SignupParams = {
+  username: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+};
+type decodedToken = {iat: number, username:string, isAdmin:string};
 
 function App() {
 
   // retrieves token, setToken from local storage and passes it down via TokenContext
   const [token, setToken] = useLocalStorage<string | null>('jobly_token', null);
+  const [user, setUser] = useState<User | null>(null);
 
-  JoblyApi.token = token;
-  // useEffect(() => {
-  //   JoblyApi.token = token;
-  // }, [token]);
+  useEffect(() => {
+    // whenever token changes, try to get the current user
+    async function tryToGetUser(token: string) {
+      try {
+        const decodedToken = jwtDecode<decodedToken>(token);
+        const foundUser = await JoblyApi.getUser(decodedToken.username);
+        setUser(foundUser);
+      } catch (err) {
+        console.error(`problem getting user: ${err}`);
+        setUser(null);
+      }
+    }
+    JoblyApi.token = token;
+    (token) ? tryToGetUser(token) : setUser(null);
+
+  }, [token]);
+
+  // logout function, passed down through CurrUserContext
+  const logout = () => {
+    setToken(null);
+  }
+
+  // login function, passed down through CurrUserContext
+  const login = async ({ username, password }: LoginParams) => {
+    try {
+      const newToken = await JoblyApi.login({username, password});
+      setToken(newToken);
+    } catch (err) {
+      console.error(`Problem logging in in app: ${err}`);
+      alert("Incorrect login");
+      throw err;
+    }
+  }
+
+  const signup = async ({username, firstName, lastName, password, email}: SignupParams) => {
+    try {
+      const newToken = await JoblyApi.signup({username, firstName, lastName, password, email});
+      setToken(newToken);
+    } catch (err) {
+      console.error(`Problem signing up in app: ${err}`);
+    }
+  }
 
 
   return (
-    <TokenContext.Provider value={[token, setToken]}>
-      <div className="App">
-        < BrowserRouter >
+    <div className="app">
+      < BrowserRouter >
+        <CurrUserContext.Provider value={[user, logout, login, signup]}>
           < NavBar />
           < AppRoutes />
-        </BrowserRouter>
-      </div>
-    </TokenContext.Provider>
+        </CurrUserContext.Provider>
+      </BrowserRouter>
+    </div>
   );
 }
 
