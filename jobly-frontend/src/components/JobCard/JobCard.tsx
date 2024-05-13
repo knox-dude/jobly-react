7dd9d7a84b6317e9e4cd1cf5433b08d6bb1ff3a7
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-13 12:51:48 -0700

    adding applications to jobcards

--- jobly-frontend/src/components/JobCard/JobCard.tsx
+++ jobly-frontend/src/components/JobCard/JobCard.tsx
@@ -1,18 +1,61 @@
-import { Card, CardBody, CardTitle, CardText } from "reactstrap"
+import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap"
 import { Job } from "@/types";
 import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import CurrUserContext from "../CurrUserContext/CurrUserContext";
+import { JoblyApi } from "@/api";
 
 // renders a JobCard, which is displayed on CompanyDetail page and JobList page
 function JobCard({id, title, salary, equity}: Job) {
 
+  const {user} = useContext(CurrUserContext);
+  const [applied, setApplied] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    setApplied(user.applications.some((application) => application == id));
+  }, [user, id])
+
+  // add application to user's jobs and set state
+  const applyToJob = async (id: string) => {
+    if (!user) {
+      return;
+    }
+    try {
+      await JoblyApi.applyToJob(user.username, id);
+      setApplied(true);
+    } catch (err) {
+      console.error(`Problem applying to job: ${err}`);
+      return;
+    }
+  }
+
+  const renderAppliedStatus = () => {
+    if (!user) {
+      return null;
+    }
+    if (applied) {
+      return (
+        <Button style={{cursor:"default"}} className="text-success m-5 h-50 bg-light">Applied!</Button>
+      );
+    } else {
+      return (
+        <Button onClick={() => applyToJob(id)} className="text-primary m-5 h-50 bg-light">Apply</Button>
+      );
+    }
+  }
+
   const navigate = useNavigate();
 
   return (
-    <Card onClick={() => navigate(`/jobs/${id}`)} style={{cursor:"pointer"}} className="job-card flex-row m-2">
-      <CardBody className="job-card-body">
+    <Card className="job-card flex-row m-2">
+      <CardBody onClick={() => navigate(`/jobs/${id}`)} style={{cursor:"pointer"}} className="job-card-body">
         <CardTitle tag="h6">{title}</CardTitle>
         <CardText>Salary: {salary}<br></br>Equity: {equity}</CardText>
       </CardBody>
+      {renderAppliedStatus()}
     </Card>
   );
 }
